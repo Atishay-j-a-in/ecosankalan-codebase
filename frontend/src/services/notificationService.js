7ec07b9357e9c7261fd82197ef1e3e77080c29f9
Atishay-j-a-in <shepherdk450@gmail.com>
2026-08-06 22:54:24 +0530

    refactor: simplify notification service by removing axios and auth headers

--- frontend/src/services/notificationService.js
+++ frontend/src/services/notificationService.js
@@ -1,21 +1,9 @@
-import axios from "axios";
-
-function authHeaders() {
-  const token = localStorage.getItem("token");
-  return token ? { Authorization: `Bearer ${token}` } : {};
-}
+import api from "./api";
 
 export async function saveFCMToken({ token, device }) {
-  return axios.post(
-    "/api/notifications/token",
-    { token, device },
-    { headers: authHeaders() }
-  );
+  return api.post("/api/notifications/token", { token, device });
 }
 
 export async function removeFCMToken(token) {
-  return axios.delete("/api/notifications/token", {
-    headers: authHeaders(),
-    data: { token },
-  });
+  return api.delete("/api/notifications/token", { data: { token } });
 }
